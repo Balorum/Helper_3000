e35be81ccea88250702c728d9443d5f4d67f5300
Alina <anomirovska@gmail.com>
2023-03-18 22:43:36 +0200

    Main Notebook Version
    The baseline for Notebook
    
    Code review pending
    
    Open for decorative and minor fixes

--- notebook_core.py
+++ notebook_core.py
@@ -1,111 +1,217 @@
 from collections import UserDict
 
 
-#create a limit for the length up to 50 symbols
-class Title():
+class Title:
 
-    def __init__(self, title):
-        self.title = title
+    def __init__(self):
+        self.__title = None
 
-#create a limit for the length up to 300 symbols
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, new_value):
+        if len(new_value) <= 50:
+            self.__title = new_value.capitalize()
+        else:
+            print('>>> Your title is longer than 50 symbols. Please, make it shorter!')
+
+
+
 class Body():
     
-    def __init__(self, body):
-        self.title = body
+    def __init__(self):
+        self.__body = None
 
-#Nikita
-class Tag():
-    pass
+    @property
+    def body(self):
+        return self.__body
 
-#create a note with title and body
+    @body.setter
+    def body(self, new_value):
+        if len(new_value) <= 300:
+            self.__body = new_value
+        else:
+            print('>>> Your note is longer than 300 symbols. Please, make it shorter!')
+
+
+
 class Note():
     
-    def __init__(self, title: Title = None, body: Body = None):
-        self.title = title
-        self.body = body
+    def __init__(self):
+        self.__note = None
+
+    @property
+    def note(self):
+        return self.__note
+
+    @note.setter
+    def note(self, new_list):
+        self.__note = new_list
 
 
-#add note to notebook
+
 class Notebook(UserDict):
 
-    def add_record(self, note):
-        self.data[note[0]] = note
+    #key: Title, value: Body -> change to Note later on
+    def add(self, note):
+        self.data[note.note[0]] = note.note[1]
+        print('>>> Note added!')
 
-def add_note():
-    print('Note added!')
 
-def search_note():
-    print('Note searched!')
+def input_error(func):
 
-def change_note():
-    print('Note edited!')
+    def inner(*args):
 
-def delete_note():
-    print('Note deleted!')
-
-def exit_helper():
-    print('Good bye!')
-
-# #decorator
-# def input_error(func):
-
-#     def inner(*args):
-
-#         try: 
-#             func(*args)
-#         except IndexError:
-#             print('Your input should be in the following order: /{command/} /{Name/} /{Phone Number/}')
-#             func(*args)
-#         except KeyError:
-#             print('Either the command or the name is wrong, please try again!')
-#         except ValueError:
-#             print('Please try again!')
-#             func(*args)
-
+        try: 
+            func(*args)
+        except IndexError:
+            print('Index Error! Please try again!')
+            func(*args)
+        except KeyError:
+            print('Key Error! Please try again!')
+            func(*args)
+        except ValueError:
+            print('Value Error! Please try again!')
+            func(*args)
             
-#     return inner
+    return inner
+
+
+    
+@input_error
+def add_note(user_input):
+    
+    title = user_input.removeprefix('add').strip().capitalize()
+    new_title = Title()
+    new_body = Body()
+    new_note = Note()
+
+    body_input = input(f'Enter content for new note \'{title}\' >>> ')
+
+    new_title.title = title
+    new_body.body = body_input
+    new_note.note = [new_title.title, new_body.body]
+
+    default_notebook.add(new_note)
+
+
+@input_error
+def search_note(user_input):
+
+    title = user_input.removeprefix('search').strip()
+    result = '>>> Note not found! Try again!'
+
+    for key in default_notebook.data.keys():
+        if title.capitalize() == key:
+            result = f'>>> The following note found: \n {key} : {default_notebook.data[key]}'
+
+    print(result)
+    
+
+@input_error
+def show_all(user_input):
+
+    print(f'>>> This is your notebook: \n {default_notebook}')
+
+
+@input_error
+def edit_note(user_input):
+
+    new_note = Body()
+    title = user_input.removeprefix('edit').strip().capitalize()
+
+    if title not in default_notebook.data.keys():
+        print('>>> Note with this name does not exist! Try again!')
+    else:
+        new_value = input(f'Enter new content for \'{title}\'>>> ')
+        new_note.body = new_value
+        default_notebook.data[title] = new_note.body
+        print('>>> Note edited!')
+
+
+@input_error
+def delete_note(user_input):
+
+    title = user_input.removeprefix('delete').strip().capitalize()
+
+    if title not in default_notebook.data.keys():
+        print('>>> Note with this name does not exist! Try again!')
+    else:
+        default_notebook.data.pop(title)
+        print('>>> Note deleted!')
+
+
+@input_error
+def help(user_input):
+
+    print(
+        """
+        You can type one of the following commands: \n
+        add [new title] - to add a new note 
+        search [title] - to find one of your notes
+        show - to see all current notes
+        edit [title] - to change a note content
+        delete [title] - to erase a note 
+        help - to see the list of commands
+        exit - to end session
+        """
+    )
+
+
+@input_error
+def exit_helper():
+
+    print('>>> Good bye!')
+
 
 COMMANDS = {
     'add': add_note,
     'search': search_note,
-    #'show': show_notes,
-    'edit': change_note,
+    'show': show_all,
+    'edit': edit_note,
     'delete': delete_note,
+    'help' : help,
     'exit': exit_helper
     }
+
 
 def handler(command_name):
     return COMMANDS[command_name]
 
-#@input_error
+
 def main():
 
     print(
         """
         You are in your notebook. Enter one of the following commands: \n
-        add ...
-        search ...
-        edit ...
-        delete ...
+        add [new title]
+        search [title]
+        show
+        edit [title]
+        delete [title]
+        help
         exit 
         """
     )
 
     while True:
 
-        user_input = input(">>> ") #add number
-        input_parsed = user_input.split() #['add', 'number']
-        command = input_parsed[0].lower() #add
+        user_input = input(">>> ")
+        input_parsed = user_input.split()
+        command = input_parsed[0].lower()
 
-        #handle exceptions
         if command not in COMMANDS.keys():
             print('Please enter one of the commands from the list!')
-        else:
-            handler(command)()
-
-        if user_input == 'exit':
+        elif command == 'exit':
             exit_helper()
             break
+        else:
+            handler(command)(user_input)
+
+
+default_notebook = Notebook()
 
 
 if __name__ == '__main__':
