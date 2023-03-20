f1517bf543e08c673ac02a75bb40950bcdc98a29
Nikita <120572806+Balorum@users.noreply.github.com>
2023-03-20 17:57:19 +0300

    Add files via upload

---
+++ bot_helper/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="Helper3000",
+    version="3",
+    description="Joint project 'Bot Assistant'",
+    url="https://github.com/Balorum/Helper_3000",
+    author="Pypeople",
+    author_email="",
+    license="MIT",
+    packages=find_namespace_packages(),
+    entry_points={"console_scripts": ["Helper3000 = bot_helper.Bot:main"]},
+)
