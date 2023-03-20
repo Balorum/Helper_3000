f6ea0b13a3897e11dc0884d6c2637a0761ea1ad6
Nikita <120572806+Balorum@users.noreply.github.com>
2023-03-20 16:40:47 +0300

    Update notebook_core.py
    Added tags and prepared the program for the console application

--- notebook_core.py
+++ notebook_core.py
@@ -1,8 +1,13 @@
 from collections import UserDict
+from copy import copy
+import json
+from os.path import isfile
+
+file_name = "Notebook.json"
+path = "./Notebook.json"
 
 
 class Title:
-
     def __init__(self):
         self.__title = None
 
@@ -15,12 +20,10 @@ class Title:
         if len(new_value) <= 50:
             self.__title = new_value.capitalize()
         else:
-            print('>>> Your title is longer than 50 symbols. Please, make it shorter!')
+            print(">>> Your title is longer than 50 symbols. Please, make it shorter!")
 
 
-
-class Body():
-    
+class Body:
     def __init__(self):
         self.__body = None
 
@@ -33,12 +36,27 @@ class Body():
         if len(new_value) <= 300:
             self.__body = new_value
         else:
-            print('>>> Your note is longer than 300 symbols. Please, make it shorter!')
+            print(">>> Your note is longer than 300 symbols. Please, make it shorter!")
 
 
+class Tag:
+    def __init__(self) -> None:
+        self.__tag = None
 
-class Note():
-    
+    @property
+    def tag(self):
+        return self.__tag
+
+    @tag.setter
+    def tag(self, value: str):
+        if value:
+            tag_list = value.split(" ")
+            self.__tag = tag_list
+        else:
+            self.__tag = []
+
+
+class Note:
     def __init__(self):
         self.__note = None
 
@@ -51,48 +69,49 @@ class Note():
         self.__note = new_list
 
 
-
 class Notebook(UserDict):
 
-    #key: Title, value: Body -> change to Note later on
+    # key: Title, value: Body -> change to Note later on
     def add(self, note):
-        self.data[note.note[0]] = note.note[1]
-        print('>>> Note added!')
+        self.data[note.note[0].title] = note
 
 
 def input_error(func):
-
     def inner(*args):
 
-        try: 
+        try:
             func(*args)
         except IndexError:
-            print('Index Error! Please try again!')
+            print("Index Error! Please try again!")
             func(*args)
         except KeyError:
-            print('Key Error! Please try again!')
+            print("Key Error! Please try again!")
             func(*args)
         except ValueError:
-            print('Value Error! Please try again!')
+            print("Value Error! Please try again!")
             func(*args)
-            
+
     return inner
 
 
-    
 @input_error
 def add_note(user_input):
-    
-    title = user_input.removeprefix('add').strip().capitalize()
+
+    title = user_input.removeprefix("add").strip().capitalize()
     new_title = Title()
     new_body = Body()
+    new_tag = Tag()
     new_note = Note()
 
-    body_input = input(f'Enter content for new note \'{title}\' >>> ')
+    body_input = input(f"Enter content for new note '{title}' >>> ")
+    tag_input = input(
+        f"Enter tags for new note '{title}' or click 'Enter' to continue without tags >>> "
+    )
 
     new_title.title = title
     new_body.body = body_input
-    new_note.note = [new_title.title, new_body.body]
+    new_tag.tag = tag_input
+    new_note.note = [new_title, new_body, new_tag]
 
     default_notebook.add(new_note)
 
@@ -100,47 +119,116 @@ def add_note(user_input):
 @input_error
 def search_note(user_input):
 
-    title = user_input.removeprefix('search').strip()
-    result = '>>> Note not found! Try again!'
+    title = user_input.removeprefix("search").strip()
+    unfor_result = ">>> Note not found! Try again!"
+    result_notes = Notebook()
 
     for key in default_notebook.data.keys():
         if title.capitalize() == key:
-            result = f'>>> The following note found: \n {key} : {default_notebook.data[key]}'
+            result_notes.data[key] = default_notebook.data[key]
+    if result_notes.data:
+        display(result_notes)
+    else:
+        print(unfor_result)
 
-    print(result)
-    
 
 @input_error
 def show_all(user_input):
+    print(f">>> This is your notebook:")
+    sort_by_alphabet(default_notebook)
 
-    print(f'>>> This is your notebook: \n {default_notebook}')
+
+def display(result_notes):
+    for val in result_notes.data.values():
+        print(f"Title: {val.note[0].title}")
+        print(f"Body: {val.note[1].body}")
+        print(f"Tags: {val.note[2].tag}\n")
+
+
+def find_by_tag(*args):
+    tags = args[0].split(" ")
+    tags_list = tags[1:]
+    result_tags = Notebook()
+    buff_note = Note()
+    for i in tags_list:
+        for val in default_notebook.data.values():
+            if val.note[2].tag:
+                if i in val.note[2].tag:
+                    buff_note.note = val.note
+                    result_tags.add(copy(buff_note))
+    sort_by_alphabet(result_tags)
+
+
+def sort_by_alphabet(notes: Notebook):
+    title_list = []
+    sorted_notes = Notebook()
+    for i in notes.data.keys():
+        title_list.append(copy(i))
+    title_list.sort()
+    for val in title_list:
+        sorted_notes.data[val] = notes.data[val]
+    display(sorted_notes)
+
+
+def save(notebook):
+    result = {}
+    with open(file_name, "w") as fh:
+        for key, val in notebook.data.items():
+            notes_list = []
+            note = {}
+            note["Title"] = val.note[0].title
+            note["Body"] = val.note[1].body
+            if val.note[2].tag:
+                note["Tags"] = val.note[2].tag
+            notes_list.append(note)
+            result[key] = notes_list
+        json.dump(result, fh, indent=4)
+
+
+def restore():
+    global default_notebook
+    with open(file_name, "r") as fh:
+        unpacked = json.load(fh)
+        for val in unpacked.values():
+            tags = Tag()
+            title = Title()
+            body = Body()
+            note = Note()
+            title.title = val[0]["Title"]
+            body.body = val[0]["Body"]
+            if "Tags" in val[0].keys():
+                tags.tag = " ".join(val[0]["Tags"])
+            else:
+                tags.tag = []
+            note.note = [title, body, tags]
+            default_notebook.data[title.title] = copy(note)
 
 
 @input_error
 def edit_note(user_input):
 
     new_note = Body()
-    title = user_input.removeprefix('edit').strip().capitalize()
+    title = user_input.removeprefix("edit").strip().capitalize()
 
     if title not in default_notebook.data.keys():
-        print('>>> Note with this name does not exist! Try again!')
+        print(">>> Note with this name does not exist! Try again!")
     else:
-        new_value = input(f'Enter new content for \'{title}\'>>> ')
+        new_value = input(f"Enter new content for '{title}'>>> ")
         new_note.body = new_value
-        default_notebook.data[title] = new_note.body
-        print('>>> Note edited!')
+        default_notebook.data[title].note[1] = new_note
+        print(">>> Note edited!")
 
 
 @input_error
 def delete_note(user_input):
 
-    title = user_input.removeprefix('delete').strip().capitalize()
+    title = user_input.removeprefix("delete").strip().capitalize()
 
     if title not in default_notebook.data.keys():
-        print('>>> Note with this name does not exist! Try again!')
+        print(">>> Note with this name does not exist! Try again!")
     else:
         default_notebook.data.pop(title)
-        print('>>> Note deleted!')
+        print(">>> Note deleted!")
 
 
 @input_error
@@ -153,6 +241,7 @@ def help(user_input):
         search [title] - to find one of your notes
         show - to see all current notes
         edit [title] - to change a note content
+        find [title] - to find notes by tags
         delete [title] - to erase a note 
         help - to see the list of commands
         exit - to end session
@@ -163,25 +252,26 @@ def help(user_input):
 @input_error
 def exit_helper():
 
-    print('>>> Good bye!')
+    print(">>> Good bye!")
 
 
 COMMANDS = {
-    'add': add_note,
-    'search': search_note,
-    'show': show_all,
-    'edit': edit_note,
-    'delete': delete_note,
-    'help' : help,
-    'exit': exit_helper
-    }
+    "add": add_note,
+    "search": search_note,
+    "show": show_all,
+    "edit": edit_note,
+    "delete": delete_note,
+    "find": find_by_tag,
+    "help": help,
+    "exit": exit_helper,
+}
 
 
 def handler(command_name):
     return COMMANDS[command_name]
 
 
-def main():
+def main_notebook():
 
     print(
         """
@@ -190,6 +280,7 @@ def main():
         search [title]
         show
         edit [title]
+        find [title]
         delete [title]
         help
         exit 
@@ -197,22 +288,24 @@ def main():
     )
 
     while True:
-
+        if isfile(path):
+            restore()
         user_input = input(">>> ")
         input_parsed = user_input.split()
         command = input_parsed[0].lower()
 
         if command not in COMMANDS.keys():
-            print('Please enter one of the commands from the list!')
-        elif command == 'exit':
+            print("Please enter one of the commands from the list!")
+        elif command == "exit":
             exit_helper()
             break
         else:
             handler(command)(user_input)
+    save(default_notebook)
 
 
 default_notebook = Notebook()
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main_notebook()
