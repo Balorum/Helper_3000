3d6a9c1ff4b0229fccf180ec058b4caba747f56b
Nikita <120572806+Balorum@users.noreply.github.com>
2023-03-22 14:59:48 +0300

    Update Bot.py

--- bot_helper/bot_helper/Bot.py
+++ bot_helper/bot_helper/Bot.py
@@ -1,19 +1,14 @@
 from bot_helper.adress_book import *
 from bot_helper.main import *
 from bot_helper.notebook_core import *
+from colorama import init, Back, Style, Fore
+
+init(autoreset=True)
 
 
 def main():
-    print(
-        """This bot has 3 functions:
-    Address book, Notebook and File sorter
-    Please Enter one of this command:
-    1) Address book
-    2) Notebook
-    3) File sorter
-    or 'exit' to exit :)
-    """
-    )
+    helper()
+
     while True:
         command = input(">>> ").split(" ")
         if command[0].lower() == "address":
@@ -22,7 +17,23 @@ def main():
             main_notebook()
         elif command[0].lower() == "file":
             path_function()
+        elif command[0].lower() == "help":
+            helper()
         elif command[0].lower() == "exit":
+            print("Good bye")
             break
         else:
             print("Such command does not exist. Try again ")
+        print("Enter the command or 'help' to see commands")
+
+
+def helper():
+    print(
+        """This bot has 3 functions:
+    Address book, Notebook and File sorter
+    Please Enter one of this command:"""
+    )
+    print(Fore.BLUE + "1) Address book")
+    print(Fore.YELLOW + "2) Notebook")
+    print(Fore.GREEN + "3) File sorter")
+    print("or 'exit' to exit :)")
